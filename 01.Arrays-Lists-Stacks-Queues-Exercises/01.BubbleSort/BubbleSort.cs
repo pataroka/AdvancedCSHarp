512f3937422dddc1064b75e8e620fa1d09540213
pataroka <predjov@yahoo.com>
2015-05-05 13:46:45 +0300

    problem1 solved

---
+++ 01.Arrays-Lists-Stacks-Queues-Exercises/01.BubbleSort/BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class BubbleSort
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int[] numbers = Array.ConvertAll(input, int.Parse);
+            bool swap = false;
+
+            do
+            {
+                swap = false;
+                Swap(numbers, ref swap);
+            } while (swap);
+
+            Console.WriteLine(string.Join(", ", numbers));
+
+        }
+
+        private static void Swap(int[] numbers, ref bool swap)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i + 1])
+                {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = temp;
+                    swap = true;
+                }
+            }
+        }
+    }
+}
