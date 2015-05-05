e398c10c3f952d96654395bbbd8d6d5ae2f4d28d
pataroka <predjov@yahoo.com>
2015-05-05 14:32:01 +0300

    prob2

--- 01.Arrays-Lists-Stacks-Queues-Exercises/01.BubbleSort/BubbleSort.cs
+++ 01.Arrays-Lists-Stacks-Queues-Exercises/01.BubbleSort/BubbleSort.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BubbleSort
+namespace _01.BubbleSort
 {
     class BubbleSort
     {
---
+++ 01.Arrays-Lists-Stacks-Queues-Exercises/02.JoinLists/JoinLists.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.JoinLists
+{
+    class JoinLists
+    {
+        static void Main(string[] args)
+        {
+            string [] input = (Console.ReadLine() + " " + Console.ReadLine()).Split();
+
+            List<int> result = new List<int>();
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                int number = int.Parse(input[i]);
+                if (!result.Contains(number))
+                {
+                    result.Add(number);
+                }
+            }
+
+            result.Sort();
+
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
