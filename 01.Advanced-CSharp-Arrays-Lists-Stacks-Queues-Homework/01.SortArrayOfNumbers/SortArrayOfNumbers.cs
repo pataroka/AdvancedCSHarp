66a486c234e9915e42e75523a9a9f1d351ea6285
pataroka <predjov@yahoo.com>
2015-05-06 12:07:07 +0300

    Prob1,2,3

---
+++ 01.Advanced-CSharp-Arrays-Lists-Stacks-Queues-Homework/01.SortArrayOfNumbers/SortArrayOfNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SortArrayOfNumbers
+{
+    class SortArrayOfNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Array.Sort(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ 01.Advanced-CSharp-Arrays-Lists-Stacks-Queues-Homework/03.CategorizeNumbers/CategorizeNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03.CategorizeNumbers
+{
+    class CategorizeNumbers
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            decimal[] numbers = Console.ReadLine().Split(' ').Select(decimal.Parse).ToArray();
+
+            List<decimal> roundNums = new List<decimal>();
+            List<decimal> floatNums = new List<decimal>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if ((numbers[i] % 1) == 0)
+                {
+                    roundNums.Add(numbers[i]);
+                }
+                else
+                {
+                    floatNums.Add(numbers[i]);
+                }
+            }
+
+            Console.WriteLine("[" + string.Join(", ", floatNums) + "] -> min: {0}, max: {1}, sum: {2}, avg: {3:F2}",
+                                floatNums.Min(), floatNums.Max(), floatNums.Sum(), floatNums.Average());
+
+            Console.WriteLine("[" + string.Join(", ", roundNums) + "] -> min: {0}, max: {1}, sum: {2}, avg: {3:F2}",
+                                roundNums.Min(), roundNums.Max(), (int)roundNums.Sum(), roundNums.Average());
+        }
+    }
+}
