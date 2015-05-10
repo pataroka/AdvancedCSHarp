424322110be26b1a3528713125a92814c83e5ac5
pataroka <predjov@yahoo.com>
2015-05-11 01:45:52 +0300

    6-9

--- 02.Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries-Homework/05.CollectTheCoins/CollectTheCoins.cs
+++ 02.Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries-Homework/05.CollectTheCoins/CollectTheCoins.cs
@@ -10,7 +10,61 @@ namespace _05.CollectTheCoins
     {
         static void Main(string[] args)
         {
+            char [][] board = new char[4][];
 
+            for (int i = 0; i < 4; i++)
+            {
+                board[i] = Console.ReadLine().ToCharArray();
+            }
+
+            char[] move = Console.ReadLine().ToCharArray();
+            int row = 0;
+            int col = 0;
+            int coins = 0;
+            int wallHits = 0;
+
+            for (int i = 0; i < move.Length; i++)
+            {
+                switch (move[i])
+                {
+                    case '<':
+                        if (col - 1 < 0)
+                        {
+                            wallHits++;
+                            continue;
+                        }
+                        col -= 1;
+                        break;
+                    case '>':
+                        if (col + 1 >= board[row].Length)
+                        {
+                            wallHits++;
+                            continue;
+                        }
+                        col += 1;
+                        break;
+                    case '^':
+                        if ((row - 1 < 0) || (col >= board[row - 1].Length))
+                        {
+                            wallHits++;
+                            continue;
+                        }
+                        row -= 1;
+                        break;
+                    default:
+                        if ((row + 1 > 3) || (col >= board[row + 1].Length))
+                        {
+                            wallHits++;
+                            continue;
+                        }
+                        row += 1;
+                        break;
+                }
+                coins += (board[row][col] == '$') ? 1 : 0;
+            }
+
+            Console.WriteLine("Coins collected: " + coins);
+            Console.WriteLine("Walls hit: " + wallHits);
         }
     }
 }
---
+++ 02.Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries-Homework/06.CountSymbols/CountSymbols.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.CountSymbols
+{
+    class CountSymbols
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            var chars = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (chars.ContainsKey(input[i]))
+                {
+                    chars[input[i]] += 1;
+                }
+                else
+                {
+                    chars.Add(input[i], 1);
+                }
+            }
+
+            var list = chars.Keys.ToList();
+            list.Sort();
+
+            foreach (var key in list)
+            {
+                Console.WriteLine("{0}: {1} time/s", key, chars[key]);
+            }
+        }
+    }
+}
