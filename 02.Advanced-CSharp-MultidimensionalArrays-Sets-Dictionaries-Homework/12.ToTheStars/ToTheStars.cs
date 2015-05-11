ba5e2598638da607ee4889b20b92b5ab6278f445
pataroka <predjov@yahoo.com>
2015-05-11 16:12:17 +0300

    all done

---
+++ 02.Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries-Homework/12.ToTheStars/ToTheStars.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _12.ToTheStars
+{
+    class ToTheStars
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            string[] firstStar = Console.ReadLine().Split(' ');
+            string[] secondStar = Console.ReadLine().Split(' ');
+            string[] thirdStar = Console.ReadLine().Split(' ');
+            double[] normandy = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            int moves = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= moves; i++)
+            {
+                if (PassStar(normandy[0], normandy[1], firstStar))
+                {
+                    Console.WriteLine(firstStar[0].ToLower());
+                }
+                else if (PassStar(normandy[0], normandy[1], secondStar))
+                {
+                    Console.WriteLine(secondStar[0].ToLower());
+                }
+                else if (PassStar(normandy[0], normandy[1], thirdStar))
+                {
+                    Console.WriteLine(thirdStar[0].ToLower());
+                }
+                else
+                {
+                    Console.WriteLine("space");
+                }
+                normandy[1] += 1.0;
+            }
+        }
+
+        static bool PassStar(double x, double y, string[] star)
+        {
+            if ((x >= double.Parse(star[1]) - 1.0) && (x <= double.Parse(star[1]) + 1.0) &&
+                   (y >= double.Parse(star[2]) - 1.0) && (y <= double.Parse(star[2]) + 1.0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 02.Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries-Homework/13.ActivityTracker/ActivityTracker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _13.ActivityTracker
+{
+    class ActivityTracker
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("bg-BG");
+
+            int lines = int.Parse(Console.ReadLine());
+            var months = new Dictionary<int, Dictionary<string, double>>();
+            
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int month = DateTime.Parse(input[0]).Month;
+                string user = input[1];
+                double distance = Double.Parse(input[2]);
+
+                if (months.ContainsKey(month))
+                {
+                    if (months[month].ContainsKey(user))
+                    {
+                        months[month][user] += distance;
+                    }
+                    else
+                    {
+                        months[month].Add(user, distance);
+                    }
+                }
+                else
+                {
+                    Dictionary<string,double> temp = new Dictionary<string, double>(){{user, distance}};
+                    months.Add(month, temp);
+                }
+            }
+
+            var sortedMonths = months.Keys.ToList();
+            sortedMonths.Sort();
+            foreach (var month in sortedMonths)
+            {
+                Console.Write(month + ": ");
+                var sortedUsers = months[month].Keys.ToList();
+                sortedUsers.Sort();
+                for (int i = 0; i < sortedUsers.Count; i++)
+                {
+                    if (i == sortedUsers.Count - 1)
+                    {
+                        Console.WriteLine(sortedUsers[i] + "(" + months[month][sortedUsers[i]] + ")");
+                    }
+                    else
+                    {
+                        Console.Write(sortedUsers[i] + "(" + months[month][sortedUsers[i]] + "), ");
+                    }
+                }
+            }
+        }
+    }
+}
