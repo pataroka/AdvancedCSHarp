1d8b945fc87507f2b14226c89a980bf0dae6b613
pataroka <predjov@yahoo.com>
2015-05-16 23:28:59 +0300

    allDone!!!

---
+++ 04.Advanced-CSharp-Regular-Expressions-Homework/08.UseYourChainsBuddy/UseYourChainsBuddy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _08.UseYourChainsBuddy
+{
+    class UseYourChainsBuddy
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex rgx = new Regex(@"<p>(.*?)<\/p>");
+            Match match = rgx.Match(input);
+            StringBuilder sb = new StringBuilder();
+
+            while (match != Match.Empty)
+            {
+                string text = Regex.Replace(match.Groups[1].Value, @"[^a-z0-9]+", " ");
+                text = Regex.Replace(text, @"\s+", " ");
+                
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] >= 97 && text[i] <= 109)
+                    {
+                        sb.Append((char)(text[i] + 13));
+                    }
+                    else if (text[i] >= 110 && text[i] <= 122)
+                    {
+                        sb.Append((char)(text[i] - 13));
+                    }
+                    else
+                    {
+                        sb.Append(text[i]);
+                    }
+                }
+                match = match.NextMatch();
+            }
+
+            Console.WriteLine(sb.ToString());
+
+        }
+    }
+}
---
+++ 04.Advanced-CSharp-Regular-Expressions-Homework/09.SemanticHTML/SemanticHTML.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _09.SemanticHTML
+{
+    class SemanticHTML
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex rgxOpen = new Regex("(\\s*)<(div).*((id|class)\\s*=\\s*\"(.*?)\").*(?=>)");
+            Regex rgxClose = new Regex("(\\s*)<\\/div>\\s.*?(\\w+)\\s*-->");
+            List<string> result = new List<string>();
+            string semantic;
+            Match match;
+
+            while (input != "END")
+            {
+                if (input.Contains("<div"))
+                {
+                    match = rgxOpen.Match(input);
+
+                    semantic = match.Value;
+                    semantic = semantic.Replace(match.Groups[2].Value, match.Groups[5].Value);
+                    semantic = semantic.Replace(match.Groups[3].Value, " ");
+                    semantic = Regex.Replace(semantic.Trim(), "\\s+", " ");
+                    semantic = match.Groups[1].Value + semantic + ">";
+                    result.Add(semantic);
+                }
+                else if (input.Contains("</div"))
+                {
+                    match = rgxClose.Match(input);
+                    semantic = match.Groups[1].Value + "</" + match.Groups[2].Value + ">";
+                    result.Add(semantic);
+                }
+                else
+                {
+                    result.Add(input);
+                }
+                input = Console.ReadLine();
+            }
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
